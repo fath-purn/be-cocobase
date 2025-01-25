467e70d346a4bc454fe0d5fe8eda153a9bb08933
Fatkhurrohaman Purnomo <talitakamilia345@gmail.com>
2025-01-25 14:25:18 +0700

    fix: format tanggal

--- controllers/pembeli.controller.js
+++ controllers/pembeli.controller.js
@@ -2,17 +2,7 @@ const prisma = require("../libs/prisma");
 const { getPagination } = require("../helpers/pagination");
 const { pembeliSchema } = require("../validations/validation");
 const { handleErrorResponse } = require("../middlewares/handleErrorResponse");
-
-function formatNomorHP(nomerHP) {
-  const nomerHPString = nomerHP.toString();
-  if (nomerHPString.startsWith("0")) {
-    return "62" + nomerHPString.slice(1);
-  } else if (!nomerHPString.startsWith("62")) {
-    return "62" + nomerHPString;
-  } else {
-    return nomerHPString;
-  }
-}
+const { formatNomorHP, formatTanggal } = require("../helpers/helper");
 
 const handleValidation = (data) => {
   const { value, error } = pembeliSchema.validate(data);
@@ -60,49 +50,60 @@ const getAllPembeli = async (req, res, next) => {
 
     const [getPembeli, { _count }] = await Promise.all([
       prisma.pembeli.findMany({
-        where: search ? {
-          nama: {
-            contains: search,
-            mode: "insensitive",
-          },
-        } : {},
+        where: search
+          ? {
+              nama: {
+                contains: search,
+                mode: "insensitive",
+              },
+            }
+          : {},
         include: {
           transaksi: {
             select: {
+              id: true,
               jumlah: true,
               harga: true,
+              createdAt: true,
+              updatedAt: true,
               produk: {
                 select: {
                   nama: true,
-                }
-              }
-            }
+                },
+              },
+            },
           },
         },
         skip,
         take,
         orderBy: {
-          createdAt: 'desc',
+          createdAt: "desc",
         },
       }),
       prisma.pembeli.aggregate({
         _count: { id: true },
-        where: search ? {
-          nama: {
-            contains: search,
-            mode: "insensitive",
-          },
-        } : {},
+        where: search
+          ? {
+              nama: {
+                contains: search,
+                mode: "insensitive",
+              },
+            }
+          : {},
       }),
     ]);
 
     const formattedPembeli = getPembeli.map((data) => ({
       ...data,
+      createdAt: formatTanggal(data.createdAt),
+      updatedAt: formatTanggal(data.updatedAt),
       transaksi: data.transaksi.map((trans) => ({
         ...trans,
+        createdAt: formatTanggal(trans.createdAt),
+        updatedAt: formatTanggal(trans.updatedAt),
         total: trans.jumlah * trans.harga,
         produk: trans.produk.nama,
-        })),
+      })),
     }));
 
     const pagination = getPagination(req, res, _count.id, page, limit, search);
@@ -124,7 +125,11 @@ const getPembeliById = async (req, res, next) => {
     const pembeli = await prisma.pembeli.findUnique({
       where: { id: toNumber(id) },
       include: {
-        transaksi: true,
+        transaksi: {
+          include: {
+            produk: true,
+          },
+        },
       },
     });
 
@@ -137,11 +142,24 @@ const getPembeliById = async (req, res, next) => {
       });
     }
 
+    const formattedPembeli = {
+      ...pembeli,
+      createdAt: formatTanggal(pembeli.createdAt),
+      updatedAt: formatTanggal(pembeli.updatedAt),
+      transaksi: pembeli.transaksi.map((trans) => ({
+        ...trans,
+        createdAt: formatTanggal(trans.createdAt),
+        updatedAt: formatTanggal(trans.updatedAt),
+        total: trans.jumlah * trans.harga,
+        produk: trans.produk.nama,
+      })),
+    };
+
     return res.status(200).json({
       success: true,
       message: "Pembeli ditemukan",
       err: null,
-      data: pembeli,
+      data: formattedPembeli,
     });
   } catch (err) {
     next(err);
@@ -210,7 +228,7 @@ const deletePembeli = async (req, res, next) => {
       });
     }
 
-    await prisma.pembeli.delete({ where: { id: toNumber(id) }, });
+    await prisma.pembeli.delete({ where: { id: toNumber(id) } });
 
     return res.status(200).json({
       success: true,
